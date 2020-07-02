c8f1348898a547e11db6cd7ad5cd0b6383893e08
Rupesh Kumar Sahu <rupeshsahunitc@gmail.com>
2020-07-02 14:41:21 +0530

    box text

--- hey.php
+++ hey.php
@@ -1,12 +1,35 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
         <title>Test page</title>
     </head>
     <body>
         <p><?php echo 'This is a <strong>test</strong>! Hi'; ?></p>
         <h1>Subhabrata</h1>
+
+
+        <div style="height:100px;width:300px;border:1px solid #ccc;font:16px/26px Georgia, Garamond, Serif;overflow:auto;">
+As you can see, once there's enough text in this box, the box will grow scroll bars... that's why we call it a scroll box! You could also place an image into the scroll box.
+
+As you can see, once there's enough text in this box, the box will grow scroll bars... that's why we call it a scroll box! Y
+
+As you can see, once there's enough text in this box, the box will grow scroll bars... that's why we call it a scroll box! Y
+
+As you can see, once there's enough text in this box, the box will grow scroll bars... that's why we call it a scroll box! Y
+
+As you can see, once there's enough text in this box, the box will grow scroll bars... that's why we call it a scroll box! Y
+
+As you can see, once there's enough text in this box, the box will grow scroll bars... that's why we call it a scroll box! Y
+
+As you can see, once there's enough text in this box, the box will grow scroll bars... that's why we call it a scroll box! Y
+
+As you can see, once there's enough text in this box, the box will grow scroll bars... that's why we call it a scroll box! Y
+
+As you can see, once there's enough text in this box, the box will grow scroll bars... that's why we call it a scroll box! Y
+
+</div>
+
     </body>
 </html>
 
